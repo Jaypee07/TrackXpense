ef688cd71bd97ed5536f47c2e7176fcc0f22650c
Johnpaul Olisaemeka <johnpaulolisa7@gmail.com>
2025-07-30 01:23:55 +0100

    Switched to environment variables and PostgreSQL

--- app/__init__.py
+++ app/__init__.py
@@ -1,27 +1,34 @@
+import os
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from flask_login import LoginManager
+from dotenv import load_dotenv
+
+load_dotenv()  # Load variables from .env
 
 db = SQLAlchemy()
 migrate = Migrate()
 login_manager = LoginManager()
 
+from app.models import User
+
+@login_manager.user_loader
+def load_user(user_id):
+    return User.query.get(int(user_id))
+
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = 'your_secret_key'
-    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///trackxpense.db'
+
+    app.config['SECRET_KEY'] = os.getenv('SECRET_KEY')
+
+    # Use DATABASE_URL from environment or default to SQLite
+    app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///trackxpense.db')
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
     db.init_app(app)
     migrate.init_app(app, db)
     login_manager.init_app(app)
-
-    # Import models *after* db is initialized
-    from .models import User, Transaction
-
-    @login_manager.user_loader
-    def load_user(user_id):
-        return User.query.get(int(user_id))
 
     from .routes import main
     app.register_blueprint(main)
